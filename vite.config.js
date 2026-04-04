f91a5aae909d06c7ae02d61aa1c2d80a6f78c657
Morgan Shepherd <morganshepherd@Morgans-MacBook-Air.local>
2026-04-04 15:15:53 -0500

    Fix eslint config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
